f476b47e5158f97965ed70de4f44a0b5fc4af778
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2017-11-20 21:07:20 +0100

    Set up basic app structure

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
@@ -12,12 +13,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
